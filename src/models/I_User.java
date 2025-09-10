a8ed1e05f32dd6c34eb8b8edab3059a68cf72a98
TCO46 <patohru2006@gmail.com>
2025-09-10 15:33:52 +0700

    init project

---
+++ src/models/I_User.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.util.List;
+
+public interface I_User {
+    boolean register(String studentID, String name, String phoneNumber, String email, String mountainCode, String tuitionFee);
+
+    boolean update(String studentID, String name, String phoneNumber, String email, String mountainCode, String tuitionFee);
+
+    List<User> getAllUser();
+
+    boolean delete(String studentID);
+
+    List<User> getUserByName(String name);
+
+    List<User> filterByCampus(String campus);
+
+
+}
---
+++ src/models/User.java
@@ -0,0 +1,82 @@
+package models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 5305293927933091843L;
+    private String StudentID;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String mountainCode;
+    private double tuitionFee;
+
+    public User(String studentID, String name, String phoneNumber, String email, String mountainCode, double tuitionFee) {
+        StudentID = studentID;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.mountainCode = mountainCode;
+        this.tuitionFee = 6000000.0;
+    }
+
+    public String getStudentID() {
+        return StudentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMountainCode() {
+        return mountainCode;
+    }
+
+    public double getTuitionFee() {
+        return tuitionFee;
+    }
+
+    public void setStudentID(String studentID) {
+        StudentID = studentID;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMountainCode(String mountainCode) {
+        this.mountainCode = mountainCode;
+    }
+
+    public void setTuitionFee(double tuitionFee) {
+        this.tuitionFee = tuitionFee;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "StudentID='" + StudentID + '\'' +
+                ", name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                ", mountainCode='" + mountainCode + '\'' +
+                ", tuitionFee=" + tuitionFee +
+                '}';
+    }
+}
