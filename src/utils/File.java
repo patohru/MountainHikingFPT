e60fc10241fa7b4bb8f4ef16dc57c795d35be2fe
TCO46 <patohru2006@gmail.com>
2025-09-10 19:28:12 +0700

    feat: add read and write file

---
+++ src/utils/File.java
@@ -0,0 +1,47 @@
+package utils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class File {
+    public static boolean writeListObjectToFile(String path, List<Object> list) throws IOException {
+        boolean result = false;
+        FileOutputStream file = new FileOutputStream(path);
+        ObjectOutputStream oos = new ObjectOutputStream(file);
+        try {
+            for(Object sm : list) {
+                oos.writeObject(sm);
+            }
+            result = true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            oos.close();
+            file.close();
+        }
+
+        return result;
+    }
+
+    public static List<Object> readFromFile(String path) throws IOException {
+        FileInputStream fis = new FileInputStream(path);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        ArrayList<Object> list = new ArrayList<>();
+
+        try {
+            Object obj = null;
+            while(fis.available() > 0) {
+                obj = (Object) ois.readObject();
+                list.add(obj);
+            }
+        } catch (ClassNotFoundException | IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            ois.close();
+            fis.close();
+        }
+
+        return list;
+    }
+}
